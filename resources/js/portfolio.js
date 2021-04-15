c1c95d5e0b51a905ce7f1c5c49425a73026664b6
saranshlakra <saranshlakra4@gmail.com>
2021-04-15 18:18:04 +0530

    jquery updated

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-  $(".js-class-feature").waypoint(function (direction) {
-    if (direction == "down") {
-      $("nav").addClass("sticky");
-    } else {
-      $("nav").removeClass("sticky");
+  $(".js-class-feature").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "10%",
     }
-  });
-
-  // var waypoint = new Waypoint({
-  //     element: document.getElementById('waypoint'),
-  //     handler: function(direction) {
-  //       console.log('Scrolled to waypoint!')
-  //     }
-  //   })
+  );
 });
